94813666d762459734ebe2bbdf8a97f37186e7ff
Showrov-CoU <45708707+Showrov-CoU@users.noreply.github.com>
2018-12-08 15:20:02 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,96 +1,137 @@
-/**Q1:Write C++ code in object oriented approach for the students.Student can be rewarded from the department
-if he gets good GPA in a semester and solved atleast 100 ACM problems in the last year.Department will publish
-the top 5 students name in their honor board. As a student of CSE, write OOP code for the project.**/
+/**Q2:Like Uber, CNG owners want to develop a software which can communicate with customer and driver.A customer needs to say
+his/her destination and nearer CNG driver will get message from the Uber company with name and mobile number of the customer.
+The driver can make a direct call and if the customer confirms the ride then driver will come to spot and carry the passenger.
+As a student of CSE, write OOP code for the project. You need to pass message from one end to another end to make communications**/
 
-#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+#include<fstream>
 using namespace std;
-
-float array[10];
-
 class Person
 {
+protected:
+    string name;
+    int national_id;
 public:
-    string Name;
-    int Age;
-
-    Person(string name, int age){this->Name=name; this->Age=age;}
+    Person(){}
+    Person(string name,int national_id){this->name=name;this->national_id=national_id;}
 };
-
-class Student:public Person
+class Driver: public Person
 {
-private:
-    float GPA;
-    int ACM_Solve;
-
+    int mobile_number;
+    string current_location;
 public:
-	Student(string n, int a, float g, int s) : Person(n,a), GPA(g), ACM_Solve(s) {}
-	void showStudent(){cout << setw(10) << Name << setw(13) <<  GPA << setw(13) << ACM_Solve << endl << endl;}
-
-	int checkGPA(float gpa)
+    double lattitude;
+    double longitude;
+    Driver (){};
+    Driver(string n,int id,int phn,string loc,double lattitude,double longitude):Person(n,id)
     {
-        if(GPA==gpa && ACM_Solve>=100)
-            return 1;
+        mobile_number=phn;
+        current_location=loc;
+        this-> lattitude=lattitude;
+        this-> longitude=longitude;
+    }
+    void driverfile ()
+    {
+        ofstream myfile2;
+        myfile2.open ("Driver Information.txt");
+        myfile2<<"\nDriver Details "<<endl;
+        myfile2<<"-------------------------------"<<endl;
+        myfile2<<"Mobile number :"<<mobile_number<<endl;
+        myfile2<<"current location   : "<<current_location<<endl;
+        myfile2<<"Lattitude :"<<lattitude<<"  degree "<<endl;
+        myfile2<<"Longitude :  "<<longitude<<" degree "<<endl;
+        myfile2<<"Name          :"<<name<<endl;
+        myfile2<<"National id   :"<<national_id<<endl;
+        myfile2<<"--------------------------------";
+        myfile2.close();
+    }
+    void booking (){cout<<"a seat has been confirmed"<<endl;}
+};
+class Uber
+{
+    Driver driver1,driver2;
+public:
+    void driver_list(Driver &driver1,Driver &driver2){this-> driver1=driver1;this-> driver2=driver2;}
 
-        else return 0;
+    double calculate_distance( double customer_lattitude, double customer_longitude, double driver_lattitude, double driver_longitude )
+    {
+      int nRadius = 6371;
+
+      double nDLat = (driver_lattitude - customer_lattitude) * (M_PI/180);
+      double nDLon = (driver_longitude-customer_longitude) * (M_PI/180);
+      double nA = pow ( sin(nDLat/2), 2 ) + cos(customer_lattitude) * cos(driver_lattitude) * pow ( sin(nDLon/2), 2 );
+      double nC = 2 * atan2( sqrt(nA), sqrt( 1 - nA ));
+      double distance = nRadius * nC;
+
+       return distance;
     }
 
-    float accessGPA()
+    void contact_with_driver(double customer_lattitude,double customer_longitude)
     {
-        return GPA;
+        double distancefromdriver1,distancefromdriver2;
+
+        distancefromdriver1=calculate_distance(customer_lattitude,customer_longitude,driver1.lattitude,driver1.longitude);
+        distancefromdriver2=calculate_distance(customer_lattitude,customer_longitude,driver2.lattitude,driver2.longitude);
+
+        if(distancefromdriver1<distancefromdriver2)
+           driver1.booking();
+        else
+           driver2.booking();
     }
 };
+class Customer:public Person
+{
+    int mobile_number;
+    string destination;
+    double lattitude;
+    double longitude;
+public:
+    Customer(){}
+    Customer(string name,int national_id,int mobile_number,string destination,double lattitude,double longitude):Person(name,national_id)
+    {
+       this-> mobile_number=mobile_number;
+        this->destination=destination;
+       this-> lattitude=lattitude;
+       this-> longitude=longitude;
+    }
+        friend class Uber;
+    void customerfile ()
+    {
+        ofstream myfile1;
+    myfile1.open ("Customer Information.txt");
+    myfile1<<"\nCustomer Details "<<endl;
+    myfile1<<"----------------------------"<<endl;
+    myfile1<<"Mobile number :"<<mobile_number<<endl;
+    myfile1<<"Lattitude :"<<lattitude<<"  degree "<<endl;
+    myfile1<<"Longitude :  "<<longitude<<" degree "<<endl;
+    myfile1<<"Destination   :  "<<destination<<endl;
+    myfile1<<"Name          :"<<name<<endl;
+    myfile1<<"National id   :"<<national_id<<endl;
+    myfile1<<"----------------------------"<<endl;
+    }
+    void contact_with_uber(Uber &uber)
+    {
+        double lattitude,longitude;
+       this-> lattitude=lattitude;
+       this-> longitude=longitude;
+        cout<<"\n\nFor customer "<<this->name<<", whose Destination is  "<<this->destination<<", ";
 
+       uber .contact_with_driver(lattitude,longitude);
+    }
+
+};
 int main()
 {
-    int i,j;
-    Student *details[8]=
-    {
-        new Student("Akash",20,3.52,45),
-        new Student("Joy",23,3.91,110),
-        new Student("Rabby",21,3.45,150),
-        new Student("Sohan",20,3.26,310),
-        new Student("Tanzir",23,3.28,250),
-        new Student("Rony",21,3.88,100),
-        new Student("Showrov",22,3.63,200),
-        new Student("Ashik",22,3.27,99)
-    };
+    Customer customer("Showrov",1,2345,"Dhaka",23,91);
+    Driver driver1("Joy",2,72567,"Tangail",21,93);
+    Driver driver2("Akash",3,8652,"Chittagong",22,91);
 
-    float temp;
-    int count=0;
-    float max;
+    driver1.driverfile();
+    customer.customerfile();
+    Uber uber;
+    uber.driver_list(driver1,driver2);
+    customer.contact_with_uber(uber);
 
-    for(i=7;i>=0;i--)
-    {
-        array[i]=details[i]->accessGPA();
-        for(j=0;j<=7-i;j++)
-        {
-            if(array[j]>array[j+1])
-            {
-                temp=array[j+1];
-                array[j+1]=array[j];
-                array[j] = temp ;
-            }
-        }
-    }
-
-    cout << "TOP 5 students on the Honor Board are:" << endl << endl<<endl;
-    cout << setw(10) << "Student Name"  << setw(10) << "GPA" << setw(17) <<  "ACM Solve" << endl  ;
-    cout << setw(10) << "____________"  << setw(10) << "___" << setw(17) << "_________" << endl << endl ;
-
-    for(i=7;i>=0;i--)
-    {
-        max=array[i];
-
-        for(j=0;j<7;j++)
-        {
-            if(details[j]->checkGPA(max)==1)
-            {
-                count++;
-                details[j]->showStudent();
-            }
-
-            if(count==5) break;
-        }
-    }
     return 0;
 }
